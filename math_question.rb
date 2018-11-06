3713fd0c9ccd2224f0d49dc919ebbf051b57c423
Jenny Poon <jenny.poon@live.ca>
2018-11-06 02:42:25 +0000

    removed other math operations and added evaluation method

--- math_question.rb
+++ math_question.rb
@@ -3,24 +3,40 @@ class MathQuestions
   def initialize
     @firstnum = rand(1...10)
     @secondnum = rand(1...10)
-    @operation = rand(1...4)
+    @operation = "+" #rand(1...4)
+  end
+    # case @operation
+    #   when 1
+    #     @operation_sign = "+"
 
-    case @operation
-      when 1
-        @operation_sign = "+"
+    #   when 2
+    #     @operation_sign = "-"
 
-      when 2
-        @operation_sign = "-"
+    #   else
+    #     @operation_sign = "x"
+    # end
 
-      else
-        @operation_sign = "x"
-    end
-end
 
   def math_question_output
-    "What is #{@firstnum} #{@operation_sign} #{@secondnum} ?"
+    "#{@firstnum}#{@operation}#{@secondnum}"
+  end
+
+  def math_evaluation
+    eval(math_question_output)
+  end
+
+  def evaluation(answer)
+    correct_result = eval(math_question_output)
+    if (correct_result == answer)
+      puts "Correct!"
+    else
+      puts "Lose a life!"
+    end
   end
 end
 
-# question = MathQuestions.new
-# p question.math_question_output
+question = MathQuestions.new
+p question.math_question_output
+p question.math_evaluation
+result = gets.chomp
+question.evaluation(result.to_i)
